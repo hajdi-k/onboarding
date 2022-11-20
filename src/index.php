c58f220a97ef9478c013be83ed029164129f41ca
Hajdi Klincharska <hajdi.kostova@gmail.com>
2022-11-20 17:57:22 +0100

    move files around

--- src/index.php
+++ src/index.php
@@ -17,25 +17,25 @@
     $allTasks = json_decode(file_get_contents("test/taskscopy.json"));
 
     // Totals
-    // include ('metrics/total-tasks.php'); // Done
-    // include ('metrics/total-tasks-by-user.php'); // Done
+    // include ('metrics/total/total-tasks.php'); // Done
+    // include ('metrics/total/total-tasks-by-user.php'); // Done
 
-    /* include ('metrics/total-completed-tasks.php');
-    include ('metrics/total-completed-tasks-by-user.php');
+    /* include ('metrics/total/total-completed-tasks.php');
+    include ('metrics/total/total-completed-tasks-by-user.php');
 
-    include ('metrics/total-overdue-tasks.php');
-    include ('metrics/total-overdue-tasks-by-user.php'); */
+    include ('metrics/total/total-overdue-tasks.php');
+    include ('metrics/total/total-overdue-tasks-by-user.php'); */
 
-    // include ('metrics/total-number-of-tasks-by-project.php'); // Done
+    // include ('metrics/total/total-number-of-tasks-by-project.php'); // Done
 
     // OverTime
 
-    // include ('metrics/new-tasks.php'); // Done
-    // include ('metrics/new-tasks-by-user.php'); // Done
+    // include ('metrics/date-range/new-tasks.php'); // Done
+    // include ('metrics/date-range/new-tasks-by-user.php'); // Done
 
-    include ('metrics/completed-tasks.php');
-    include ('metrics/completed-tasks-by-user.php');
+    include ('metrics/date-range/completed-tasks.php');
+    include ('metrics/date-range/completed-tasks-by-user.php');
 
-    /* include ('metrics/overdue-tasks.php');
-    include ('metrics/overdue-tasks-by-user.php'); */
+    /* include ('metrics/date-range/overdue-tasks.php');
+    include ('metrics/date-range/overdue-tasks-by-user.php'); */
 ?>
